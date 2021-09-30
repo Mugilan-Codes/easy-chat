9ff7021d6c1631308b2b87184aca33e92aa7a939
Mugilan <mugilancodes@gmail.com>
2021-09-30 16:37:45 +0530

    theme check

--- App/index.js
+++ App/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {enableScreens} from 'react-native-screens';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import styled from 'styled-components/native';
 
 import StateProvider from './contexts/StateProvider';
+
+const StyledView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: ${props => props.theme.colors.background};
+`;
+const StyledText = styled.Text`
+  color: ${props => props.theme.colors.primary};
+`;
 
 enableScreens();
 const App = () => {
@@ -12,9 +22,9 @@ const App = () => {
     <SafeAreaProvider>
       <StateProvider>
         <NavigationContainer>
-          <View>
-            <Text>Entry Point</Text>
-          </View>
+          <StyledView>
+            <StyledText>Entry Point</StyledText>
+          </StyledView>
         </NavigationContainer>
       </StateProvider>
     </SafeAreaProvider>
