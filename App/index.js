e5399555e78cc5c39fdebaa1b3a3514238afb052
Mugilan <mugilancodes@gmail.com>
2021-09-30 13:51:50 +0530

    :tada: initial commit

---
+++ App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Entry Point</Text>
+    </View>
+  );
+};
+
+export default App;
